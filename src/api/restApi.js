d1dcf0d948c6f3065522bce7a0873039a65b59d0
Nick Malezh <n.malezh@gmail.com>
2017-12-10 13:00:23 +0300

    restApi url fixed

--- src/api/restApi.js
+++ src/api/restApi.js
@@ -4,9 +4,9 @@ const BASE_URL = 'http://localhost:3000'
 
 export default {
     addProjectToCompetition(project){
-        return axios.post(BASE_URL + '/add', project)
+        return axios.post(BASE_URL + '/participant/add', project)
     },
     publishProjects(){
-        return axios.get(BASE_URL + '/publish')
+        return axios.get(BASE_URL + '/participant/publish')
     },
 }
